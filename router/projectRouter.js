bb469e00fa0f1ceb3f2c0447a013c3718e1b0ffd
fikri <fikri@fikri.com>
2020-02-09 21:01:21 +0700

    filter backend

--- router/projectRouter.js
+++ router/projectRouter.js
@@ -6,5 +6,7 @@ const {auth} = require('./../helpers/jwt')
 router.post('/create-ads',auth,projectController.createAds);
 router.get('/get-ongoing-ads/:id_user',auth,projectController.getAdsOngoing);
 router.get('/get-history-ads/:id_user',auth,projectController.getAdsHistory);
-router.get('/get-all-ads-ongoing',auth,projectController.getAllAdsOnGoing);
+router.post('/get-all-ads-ongoing',auth,projectController.getAllAdsOnGoing);
+router.get('/get-all-category',auth,projectController.getAllCategoris);
+router.get('/get-all-kabupaten',auth,projectController.getAllKabupaten);
 module.exports = router;
