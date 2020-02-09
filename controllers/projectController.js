7bbf8c2c1f23f8e9f43ed40dcdd0080409864f3f
fikri <fikri@fikri.com>
2020-02-09 12:10:37 +0700

    get endorse

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -33,5 +33,18 @@ module.exports = {
                 res.send(result)   
             }
         })
+    },
+    getAllAdsOnGoing : (req,res) => {
+        var sql = `select p.id as id, p.id_user as id_user,p.product_name,p.file as file, p.upload_at as upload_at, 
+        location_ads,sex_ads, age_ads, description,estimation_ads, created_ads, status_ads,u.fullname as ads_creator,u.email as email_creator,
+        c.name as category_name
+        from project_ads p 
+        join users u on p.id_user = u.id
+        join category_ads c on c.id = p.category where status_ads != 5 order by id desc;`
+
+        conn.query(sql,(err,result) => {
+            if(err) throw err
+            res.send(result)
+        })
     }
 }
--- router/projectRouter.js
+++ router/projectRouter.js
@@ -6,4 +6,5 @@ const {auth} = require('./../helpers/jwt')
 router.post('/create-ads',auth,projectController.createAds);
 router.get('/get-ongoing-ads/:id_user',auth,projectController.getAdsOngoing);
 router.get('/get-history-ads/:id_user',auth,projectController.getAdsHistory);
+router.get('/get-all-ads-ongoing',auth,projectController.getAllAdsOnGoing);
 module.exports = router;
