550b53e2018d0bfd661024156a8875d0db15ea14
fikar46 <mzulfikarmey@gmail.com>
2021-08-18 13:43:35 +0700

    backend diendorse

--- database/index.js
+++ database/index.js
@@ -1,13 +1,22 @@
 const mysql = require('mysql');
 
+// const conn = mysql.createPool({
+//     // localhost aktif ketika menggunakan cara proxy selain unix
+//     // host:'localhost', 
+//     user:'root',
+//     password:'fikar123',
+//     database:'diendorse',
+//     // port aktif ketika menggunakan cara proxy selain unix
+//     // port: 3308,
+//     socketPath: `/cloudsql/diendorse:asia-southeast1:diendorse`,
+// });  
+// module.exports = conn;
+
 const conn = mysql.createPool({
-    // localhost aktif ketika menggunakan cara proxy selain unix
-    // host:'localhost', 
+    host:'localhost',
     user:'root',
     password:'fikar123',
     database:'diendorse',
-    // port aktif ketika menggunakan cara proxy selain unix
-    // port: 3308,
-    socketPath: `/cloudsql/diendorse:asia-southeast1:diendorse`,
+    port: 3306
 });  
 module.exports = conn;
